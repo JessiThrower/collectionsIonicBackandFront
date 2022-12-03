0d790166964a138014ef629287fc4b789f01b96c
Jessica CA <jessicastalm@hotmail.com>
2022-12-03 22:50:39 +0000

    task completed

---
+++ frontend/collection/src/app/model/boardgame.spec.ts
@@ -0,0 +1,7 @@
+import { Boardgame } from './boardgame';
+
+describe('Boardgame', () => {
+    it('should create an instance', () => {
+        expect(new Boardgame()).toBeTruthy();
+    });
+});
---
+++ frontend/collection/src/app/model/boardgame.ts
@@ -0,0 +1,9 @@
+export class Boardgame {
+    id?: number;
+    name: string;
+    type: string;
+    company?: string;
+    players?: number;
+    description?: string;
+    year?: number;
+}
